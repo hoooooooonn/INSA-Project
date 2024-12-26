e080cbf472d6543b71dcf7e9a85048897e34d897
mw7813 <mw7813@naver.com>
2024-12-26 21:16:12 +0900

    15615

--- INSAProject/src/main/java/com/min/edu/dto/StudentDto.java
+++ INSAProject/src/main/java/com/min/edu/dto/StudentDto.java
@@ -2,11 +2,40 @@ package com.min.edu.dto;
 
 import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@AllArgsConstructor
+
+
 public class StudentDto implements Serializable {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1586032342560045184L;
+
+	private static final long serialVersionUID = 8829006345846422338L;
+	
+	private int stu_id, age;
+	private String stu_name, stu_phone, gen, pref, note ;
+	
+	public StudentDto(int age, String stu_name, String stu_phone, String gen, String pref, String note) {
+		super();
+		this.stu_name = stu_name;
+		this.age = age;
+		this.stu_phone = stu_phone;
+		this.gen = gen;
+		this.pref = pref;
+		this.note = note;
+	}
+	
+	
 
 }
